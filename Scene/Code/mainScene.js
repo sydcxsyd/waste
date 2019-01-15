21c90a21a31a2e70b8f62655efdecf35f595d5ae
sudiop <sydcxsyd@users.noreply.github.com>
2019-01-15 22:17:10 +0800

    com

---
+++ Scene/Code/mainScene.js
@@ -0,0 +1,21 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        pageNode : cc.Node,
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+
+    // called every frame
+    update: function (dt) {
+
+    },
+
+    onClickThrow (){
+
+    },
+});
