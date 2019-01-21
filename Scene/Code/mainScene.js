5d8252772f8feb98d00e8c7e39d12bfe4466f054
sudiop <sydcxsyd@users.noreply.github.com>
2019-01-22 00:36:48 +0800

    daliy

--- Scene/Code/mainScene.js
+++ Scene/Code/mainScene.js
@@ -4,6 +4,17 @@ cc.Class({
     properties: {
         pageNode : cc.Node,
         inputBox : cc.EditBox,
+
+        gotPage : cc.Node,
+        enterPage : cc.Node,
+        historyPage : cc.Node,
+
+        brilliantLabel : cc.Label,  //智商
+        strongLabel : cc.Label,     //打架能力
+        talkLabel : cc.Label,       //bb能力 骂娘能力 喷人能力
+        speedLabel : cc.Label,      //逃跑速度
+        bigLabel : cc.Label ,       //容量
+        fightLabel : cc.Label ,       //容量
     },
 
     // use this for initialization
@@ -27,4 +38,14 @@ cc.Class({
             console.log("雅蠛蝶")
         })
     },
+
+    onClickWriteBtn (){
+        this.enterPage.active = true;
+    },
+
+    onClickHistoryBtn(){
+        this.historyPage.active = true;
+    },
+
+
 });
